5badf850598d5f4980e7953ec4078aaaf5a2e218
xiaoch05 <xiaoch2010@gmail.com>
2026-05-25 19:43:12 +0800

    update script

--- src/subgraph/uniswapv4/src/UniswapV4Pool.ts
+++ src/subgraph/uniswapv4/src/UniswapV4Pool.ts
@@ -9,15 +9,31 @@ export async function handleSwap(event: Swap): Promise<void> {
     const id = `${event.context.chainId}-${event.params.id}`;
     const now = `${Date.now() / 1000}`;
 
-    let memEntity = UniswapV4Slot0.memLoad(id);
-    if (memEntity == null) {
-        return;
+    // save db
+    {
+        let entity = await UniswapV4Slot0.load(id);
+        if (entity === null) {
+            entity = new UniswapV4Slot0(id);
+        }
+        entity.chainId = BigInt(event.context.chainId);
+        entity.pool = event.params.id;
+        entity.sqrtPriceX96 = event.params.sqrtPriceX96.toString();
+        entity.liquidity = event.params.liquidity.toString();
+        entity.tick = event.params.tick.toString();
+        await entity.save();
     }
-    memEntity.chainId = event.context.chainId;
-    memEntity.pool = event.params.id;
-    memEntity.tick = event.params.tick.toString();
-    memEntity.sqrtPriceX96 = event.params.sqrtPriceX96.toString();
-    memEntity.liquidity = event.params.liquidity.toString();
-    memEntity.timestamp = now;
-    memEntity.memSave();
+    // save memory
+    {
+        let memEntity = UniswapV4Slot0.memLoad(id);
+        if (memEntity == null) {
+            return;
+        }
+        memEntity.chainId = event.context.chainId;
+        memEntity.pool = event.params.id;
+        memEntity.tick = event.params.tick.toString();
+        memEntity.sqrtPriceX96 = event.params.sqrtPriceX96.toString();
+        memEntity.liquidity = event.params.liquidity.toString();
+        memEntity.timestamp = now;
+        memEntity.memSave();
+    }
 }
